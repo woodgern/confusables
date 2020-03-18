a299875bbc0b21a79986afbdde6845b3089fdbb2
TrustedMercury <kevin.jt2007@gmail.com>
2020-03-17 20:02:51 -0400

    Added PEP8 conventions to setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     version="1.1.1",
     author="Nathan Woodger",
     author_email="woodgern@gmail.com",
-    description="A python package providing functionality for matching words that can be confused for eachother, but contain different characters",
+    description="A python package providing functionality for matching words that can be confused for each other, but contain different characters",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/woodgern/confusables",
@@ -20,4 +20,3 @@ setuptools.setup(
     ],
     package_data={'confusables': ['assets/*.json', 'assets/*.txt']},
 )
-
